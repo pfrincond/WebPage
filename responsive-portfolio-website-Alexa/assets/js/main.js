a415c4d4781b6ffb9010711fe73b5049bfd79ac8
Pablo Rincón <fernando.rincond@gmail.com>
2021-10-20 18:16:54 -0500

    Adding qualifications section

--- responsive-portfolio-website-Alexa/assets/js/main.js
+++ responsive-portfolio-website-Alexa/assets/js/main.js
@@ -34,13 +34,13 @@ const skillsContent = document.getElementsByClassName('skills__content');
 const skillsHeader = document.querySelectorAll('.skills__header');
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className;
+    const itemClass = this.parentNode.className;
 
-    for (i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close'
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
     }
     if (itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skills__content skills__open'
+        this.parentNode.className = 'skills__content skills__open';
     }
 }
 
