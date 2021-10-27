db29cc4d1352e22ceeece3b7ac563bc380906bb8
Pablo Rincón <fernando.rincond@gmail.com>
2021-10-27 17:29:54 -0500

    adding position relative for the portfolio__container property

--- responsive-portfolio-website-Alexa/assets/js/main.js
+++ responsive-portfolio-website-Alexa/assets/js/main.js
@@ -92,7 +92,8 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+
+let swiper = new window.Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
